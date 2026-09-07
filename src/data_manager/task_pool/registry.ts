beeff565301946ccbc51a96577769debe1de023b
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-09-06 22:18:28 -0700

    various docs and new get method

--- src/data_manager/task_pool/registry.ts
+++ src/data_manager/task_pool/registry.ts
@@ -3,6 +3,27 @@ import type { Task, TaskResult } from "./task.js";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const TASK_HANDLERS = new Map<string, Task<string, any[], any>>();
 
+/**
+ * Combines a `string` task `id` and a `handler` into a `Task` that can be registered and called with a `TaskPool`.
+ *
+ * Modules that define tasks that can be run on a worker should wrap the task handler in this function, then export the
+ * resulting `Task`'s type while passing its value to `registerTask`:
+ *
+ * ```typescript
+ * const addTask = task("add", (a: number, b: number) => a + b);
+ * export type AddTask = typeof addTask;
+ * registerTask(addTask);
+ * ```
+ *
+ * Main thread code can then import the task's type to create a `TaskHandle`:
+ *
+ * ```typescript
+ * import type { AddTask } from "./add_task.js";
+ * const addTask = taskHandle<AddTask>("add");
+ * const pool = new TaskPool();
+ * const three = await pool.runTask(addTask, 1, 2);
+ * ```
+ */
 export const task = <Id extends string, In extends unknown[], Out>(
   id: Id,
   handler: (...args: In) => TaskResult<Out>
