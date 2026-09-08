f8698b45ab5dd608593cc7b61fd88958aac0b164
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-09-07 17:59:08 -0700

    use a more specific type

--- src/data_manager/task_pool/task.ts
+++ src/data_manager/task_pool/task.ts
@@ -1,3 +1,5 @@
+import type { ErrorObject } from "serialize-error";
+
 import type { NumberType, TypedArray } from "../../types.js";
 import EventDispatcher from "../../EventDispatcher.js";
 
@@ -146,6 +148,6 @@ export type WorkerResponse<T extends Task = Task> = {
     }
   | {
       error: true;
-      result: unknown;
+      result: ErrorObject;
     }
 );
