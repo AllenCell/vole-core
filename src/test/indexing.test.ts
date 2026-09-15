42373c13fe06847fe688e16c483a2a31f6384ceb
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-09-15 15:51:18 -0700

    tests for dimension reordering

--- src/test/indexing.test.ts
+++ src/test/indexing.test.ts
@@ -1,45 +1,25 @@
 import { setFromChunk } from "../data_manager/indexing.js";
-import type { TypedArray } from "../types.js";
 
 const src = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 
 const srcShape = [2, 2, 2];
 
-/** Count calls to `set` on this array and any subarrays created from it */
-const countSetsRecursive = (arr: TypedArray, counter = { count: 0 }): { count: number } => {
-  const originalSubarray = arr.subarray.bind(arr);
-  const originalSet = arr.set.bind(arr);
-
-  vi.spyOn(arr, "subarray").mockImplementation(((...args) => {
-    const result = originalSubarray(...args);
-    countSetsRecursive(result, counter);
-    return result;
-  }) as typeof originalSubarray);
-
-  vi.spyOn(arr, "set").mockImplementation((...args) => {
-    originalSet(...args);
-    counter.count += 1;
-  });
-
-  return counter;
-};
-
 describe("setFromChunk", () => {
   it("can copy a chunk into a destination array of equal size", () => {
     const params = { srcShape, destShape: srcShape, offset: [0, 0, 0] };
     const dest = new Uint8Array(8).fill(0);
-    const setCounter = countSetsRecursive(dest);
+    const set = vi.spyOn(dest, "set");
 
     setFromChunk(src, dest, params);
 
     expect(dest).toEqual(src);
-    expect(setCounter.count).toEqual(1);
+    expect(set).toHaveBeenCalledTimes(1);
   });
 
   it("can copy a chunk into an n-dimensional subchunk of a larger array", () => {
     const params = { srcShape, destShape: [4, 4, 4], offset: [2, 2, 2] };
     const dest = new Uint8Array(64).fill(0);
-    const setCounter = countSetsRecursive(dest);
+    const set = vi.spyOn(dest, "set");
 
     setFromChunk(src, dest, params);
 
@@ -50,13 +30,13 @@ describe("setFromChunk", () => {
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 3, 4,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 6, 0, 0, 7, 8,
     ]));
-    expect(setCounter.count).toEqual(4);
+    expect(set).toHaveBeenCalledTimes(4);
   });
 
   it("consolidates dimensions to reduce `set` calls", () => {
     const params = { srcShape, destShape: [4, 4, 2], offset: [2, 0, 0] };
     const dest = new Uint8Array(32).fill(0);
-    const setCounter = countSetsRecursive(dest);
+    const set = vi.spyOn(dest, "set");
 
     setFromChunk(src, dest, params);
 
@@ -67,6 +47,34 @@ describe("setFromChunk", () => {
       1, 2, 3, 4, 0, 0, 0, 0,
       5, 6, 7, 8, 0, 0, 0, 0
     ]));
-    expect(setCounter.count).toEqual(2);
+    expect(set).toHaveBeenCalledTimes(2);
+  });
+
+  it("reorders dimensions when copying into a destination array of equal size", () => {
+    const params = { srcShape, destShape: srcShape, offset: [0, 0, 0], dimensionOrder: [1, 0, 2] };
+    const dest = new Uint8Array(8).fill(0);
+    const set = vi.spyOn(dest, "set");
+
+    setFromChunk(src, dest, params);
+
+    expect(dest).toEqual(new Uint8Array([1, 2, 5, 6, 3, 4, 7, 8]));
+    expect(set).toHaveBeenCalledTimes(4);
+  });
+
+  it("reorders dimensions when copying into a subchunk of a larger array", () => {
+    const params = { srcShape, destShape: [4, 4, 4], offset: [2, 0, 2], dimensionOrder: [2, 1, 0] };
+    const dest = new Uint8Array(64).fill(0);
+    const set = vi.spyOn(dest, "set");
+
+    setFromChunk(src, dest, params);
+
+    // prettier-ignore
+    expect(dest).toEqual(new Uint8Array([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 1, 5, 0, 0, 3, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 2, 6, 0, 0, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]));
+    expect(set).toHaveBeenCalledTimes(8);
   });
 });
